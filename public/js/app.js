7789c1b60641c00a8b694c1d7db809b92f138464
Jazmmine <jazmine.angoma@gmail.com>
2016-11-14 18:23:10 -0500

    agregando vista de seleccionar figura

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#start-game").click(function(e){
+        e.preventDefault();
+        $(".fondo-presentacion").slideUp(1000);
+        $(".fondo-choose").slideDown(1000);
+    });
+});
