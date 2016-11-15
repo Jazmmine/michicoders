363f0bfc1ac1b5d0bba2704a6a6067045664df79
diananr <navarroruiz.diana>
2016-11-15 00:26:42 -0500

    agregando socket.io

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,20 @@
-$(document).ready(function(){
-    $("#start-game").click(function(e){
+var socket = io();
+
+$(document).ready(function() {
+	$("#start-game").click(function(e){
         e.preventDefault();
         $(".fondo-presentacion").slideUp(1000);
         $(".fondo-choose").slideDown(1000);
     });
+
+	$(".cuadrado").click(function(){
+		var $cruz = $("<i></i>").text("clear");
+		$cruz.addClass("cursor-pointer color-white material-icons large");
+
+		socket.emit("conectando", $cruz);
+	});
+});
+
+socket.on("yajugo", function (data) {
+	$(".cuadrado").append(data);
 });
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-var express = require("express");
+var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-app.use(express.static(__dirname + "/public"));
-app.listen(1511, function () {
-	console.log("listen:1511");
+app.use(express.static(__dirname + '/public'));
+
+io.on("coneccion", function(socket) {
+	socket.on("turnando", function (data) {
+		socket.broadcast.emit("yajugo", data)
+	});
+});
+
+server.listen(1511, function() {
+	console.log("El servidor ha iniciado en el puerto 1511");
 });
